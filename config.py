1ef4401a23b1830fba9d915761f6f55fba97beec
pangkun248 <39581901+pangkun248@users.noreply.github.com>
2022-07-14 20:40:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 class Config:
-    # 训练集路径,验证集路径(mAP相关)
+    # 训练集路径,验证集路径
     train_dir = 'VOC2007文件夹路径'
     val_dir = 'VOC2007文件夹路径'
-    # 图片最大与最小输入长宽尺寸
+    # 最大和最小输入尺寸
     max_size = 1000
     min_size = 600
-    num_workers = 8         # 取决于你的cpu核数
-    test_num_workers = 8    # 同上
 
     # 计算loss时rpn与roi所占的比重
     rpn_sigma = 3.
@@ -22,8 +20,8 @@ class Config:
     nms_roi = 0.3   # ROI阶段的nms阈值 仅存在于非训练阶段,即训练阶段整个网络只有一个rpn阶段的nms,而非训练阶段则有两个
     use_sgd = True  # 是否使用SGD优化方式
     load_path = r''  # 基于此模型权重训练
-    # 注意Faster-RCNN中是由背景这一类的,但是这里及xml2txt都没有 '__background__'这一类,是因为Faster-RCNN在内部临时把所有的
-    # target_label都+1,计算ap以及最后nms的时候又跳过label等于0的情况. 详情参见 ProposalTargetCreator 类 以及_suppress方法
+    # 注意Faster-RCNN中是有背景这一类的,但是这里及xml2txt都没有 '__background__'这一类,是因为Faster-RCNN在内部临时把所有的
+    # label都+1,计算ap以及最后nms的时候又跳过label等于0的情况. 详情参见 ProposalTargetCreator 类 以及_suppress方法
     class_name = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
                                   'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
 
